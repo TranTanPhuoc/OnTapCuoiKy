ff9bb225975f6ab261cbf271f1a083d7ec6b4568
Tran Tan Phuoc <trantanphuoc262@gmail.com>
2022-05-26 22:18:27 +0700

    done Sync Login

--- app/src/main/java/com/example/onthicuoiky/MainActivity.java
+++ app/src/main/java/com/example/onthicuoiky/MainActivity.java
@@ -10,14 +10,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.onthicuoiky.dao.UserDao;
+import com.example.onthicuoiky.database.AppdataBase;
+import com.example.onthicuoiky.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private EditText txtEmail,txtPassword;
+    private DatabaseReference mDatabase;
+    private UserDao userDAO;
+    private List<User> users;
     private Button btnLogin,btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnSignIn_Login);
         btnRegister = findViewById(R.id.btnRegister_Login);
         auth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        //Room database
+        userDAO = AppdataBase.getDatabase(this).UserDAO();
+        getAllUserFromFirebase();
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,8 +55,11 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
+                            User user = userDAO.findByEmail(email);
                             Intent intent = new Intent(MainActivity.this,ListMonHoc.class);
+                            intent.putExtra("userId", user.getId());
                             startActivity(intent);
+                            Toast.makeText(MainActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
                             finishAffinity();
                         }
                         else{
@@ -56,4 +77,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    private void getAllUserFromFirebase() {
+
+        mDatabase.child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userDAO.delete();
+                for(DataSnapshot sn : snapshot.getChildren()) {
+                    User user = sn.getValue(User.class);
+                    userDAO.insert(user);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
