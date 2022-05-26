db0b702d9356d878ceb6d885432e5794aef963bf
Tran Tan Phuoc <trantanphuoc262@gmail.com>
2022-05-26 17:52:27 +0700

    xử lí Login (done)

--- app/src/main/java/com/example/onthicuoiky/MainActivity.java
+++ app/src/main/java/com/example/onthicuoiky/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.onthicuoiky;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth auth;
+    private EditText txtEmail,txtPassword;
+    private Button btnLogin,btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtEmail = findViewById(R.id.txtEmailLogin);
+        txtPassword = findViewById(R.id.txtPassLogin);
+        btnLogin = findViewById(R.id.btnSignIn_Login);
+        btnRegister = findViewById(R.id.btnRegister_Login);
+        auth = FirebaseAuth.getInstance();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email  = txtEmail.getText().toString().trim();
+                String passWord = txtPassword.getText().toString().trim();
+                auth.signInWithEmailAndPassword(email,passWord).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent intent = new Intent(MainActivity.this,ListMonHoc.class);
+                            startActivity(intent);
+                            finishAffinity();
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RegisterMain.class);
+                startActivity(intent);
+            }
+        });
     }
 }
